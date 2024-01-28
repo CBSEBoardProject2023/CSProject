d6ed55af987fc6dd9ad4d8f8c79d882ae023b40f
Hixon1000 <86545468+hixon1000@users.noreply.github.com>
2024-01-28 11:03:44 +0400

    Update main.py
    Few changes

--- main.py
+++ main.py
@@ -1,91 +1,96 @@
 import FinalCscProject as cs
-test = cs.project()
-
+main = cs.project()
+Logged_in = main.logged_in
 def employee_add():
     name = input("Enter employee name : ")
-    age = test.force_int("Enter employee age : ")
+    age = main.force_int("Enter employee age : ")
     occupation = input("Enter occupation : ")
-    date_of_joining = test.force_date("Enter date of joining (yyyy-mm-dd) : ")
+    date_of_joining = main.force_date("Enter date of joining (yyyy-mm-dd) : ")
     contact_info = input("Enter contact information : ")
-    salary = test.force_int("Enter the salary : ")
-    test.employeedetails(name,age,occupation,date_of_joining,contact_info,salary)
+    salary = main.force_int("Enter the salary : ")
+    main.employeedetails(name,age,occupation,date_of_joining,contact_info,salary)
 def display_employee_record():
-    test.display_emp_details(input("Enter employee name : "))
+    main.display_emp_details(input("Enter employee name : "))
 def update_employee_records():
     name = input("Enter employee name : ")
+    print("Possible feilds : name, age, occupation, date_of_joining, contact_info, salary")
     field = input("Enter the feild to change : ")
     value = input("Enter changed value : ")
-    test.update_emp_record(name,field,value)
+    main.update_emp_record(name,field,value)
 def delete_employee_records():
     name = input("Enter employee name : ")
-    test.delete_record(name)
+    main.delete_record(name)
 def book_add():
     name = input("Enter book name : ")
     author = input("Enter author name : ")
     publisher = input("Enter publisher name : ")
-    purchase_date = test.force_date("Enter purchase date (yyyy-mm-dd) : ")
-    purchase_cost = test.force_int("Enter purchase cost of book : ")
-    copies = test.force_int("Enter the number of copies : ")
-    test.bookdetails(name,author,publisher,purchase_date,purchase_cost,copies)
+    purchase_date = main.force_date("Enter purchase date (yyyy-mm-dd) : ")
+    purchase_cost = main.force_int("Enter purchase cost of book : ")
+    copies = main.force_int("Enter the number of copies : ")
+    main.bookdetails(name,author,publisher,purchase_date,purchase_cost,copies)
 def book_display():
-    test.display_book_details(input("Enter book name"))
+    main.display_book_details(input("Enter book name : "))
 def book_update():
     name = input("Enter book name : ")
+    print("Possible feilds : book_id, name, author, publisher, date_of_purchase, cost_of_purchase, copies")
     field = input("Enter the feild to change : ")
     value = input("Enter changed value : ")
-    test.update_book_record(name, field,value)
+    main.update_book_record(name, field,value)
 def book_delete():
     name = input("Enter book name : ")
-    test.delete_book_record(name)
+    main.delete_book_record(name)
 def purchase_add():
     name = input("Enter customer name : ")
     contact = input("Enter contact number : ")
     book_name = input("Enter book name : ")
     address = input("Enter your address : ")
     status = input("Enter book stats (Purchased or issued) : ")
-    purchase_date = test.force_int("Enter the purchase date : ")
-    cost = test.force_int("Enter the cost : ")
-    date_of_return = "Null"
+    purchase_date = main.force_date("Enter the purchase date (yyyy-mm-dd) : ")
+    cost = main.force_int("Enter the cost : ")
     while True:
-        n = input("Is the customer registered?")
-        if n in "Yy":
-            username = input("Enter customer username : ")
-            test.purchdetails(name,book_name,contact,address,status,purchase_date,cost,date_of_return,username)
-            break
-        elif n in "Nn":
-            test.purchdetails(name,book_name,contact,address,status,purchase_date,cost,date_of_return)
+        date = input("Is there a date of return (Y/n) : ")
+        if date in "Yy":
+            date_of_return = main.force_date("Enter date of return : ")
+            main.purchdetails(name,book_name,contact,address,status,purchase_date,cost,date_of_return)
             break
         else:
-            print("Invalid input")
+            main.purchdetails(name,book_name,contact,address,status,purchase_date,cost)
+            break
 def purchase_display():
-    test.display_purch_details(input("Enter customer name : "))
+    main.display_purch_details(input("Enter customer name : "))
 def purchase_update():
     name = input("Enter customer name : ")
+    print("Possible feilds : sl_no, name, contact_info, address, status, date_of_rent_or_purchase, cost_of_rent_or_purchase, date_of_return, Book_id, User_id")
     field = input("Enter the feild to change : ")
     value = input("Enter changed value : ")
-    test.update_purch_record(name, field,value)
+    main.update_purch_record(name, field,value)
 def purchase_delete():
-    test.delete_purch_record(input("Enter customer name : "))
+    main.delete_purch_record(input("Enter Purchase ID : "))
 def user_add():
-    name = input("Enter customer name : ")
+    name = input("Enter employee/represntative name : ")
     user_name = input("Enter a new username : ")
     password = input("Enter the password : ")
-    test.userdetails(name,user_name,password)
+    emp_id = main.force_int("Enter employee id : ")
+    main.userdetails(name,user_name,password,emp_id)
 def user_display():
-    test.display_user_details(input("Enter the username : "))
+    main.display_user_details(input("Enter the username : "))
 def user_update():
     name = input("Enter user name : ")
+    print("Possible feilds : user_id, user_name, username, password, emp_id")
     field = input("Enter the feild to change : ")
     value = input("Enter changed value : ")
-    test.update_user_details(name, field, value)
+    main.update_user_details(name, field, value)
 def user_delete():
-    test.delete_user_record(input("Enter the username : "))
-print("Welcome to our program!")
-while True:
+    main.delete_user_record(input("Enter the username : "))
+if Logged_in:
+    print("Welcome to library management system!")
+while Logged_in:
     print("1)Employee management")
     print("2)Inventory of books")
-    print("3)Exit")
-    choice = test.force_int("Enter your choice : ")
+    print("3)Purchase information")
+    print("4)User management (Mostly for admin)")
+    print("5)Exit")
+    choice = main.force_int("Enter your choice : ")
     if choice == 1:
         print("Entered employee management")
         while True:
@@ -94,7 +99,7 @@ while True:
             print("3)Update employee information")
             print("4)Remove an employee")
             print("5)Enter back to main")
-            choice = test.force_int("Enter your choice : ")
+            choice = main.force_int("Enter your choice : ")
             if choice == 1:
                 employee_add()
             elif choice == 2:
@@ -115,7 +120,7 @@ while True:
             print("3)Update book information")
             print("4)Remove a book")
             print("5)Enter back to main")
-            choice = test.force_int("Enter your choice : ")
+            choice = main.force_int("Enter your choice : ")
             if choice == 1:
                 book_add()
             elif choice == 2:
@@ -136,6 +141,7 @@ while True:
             print("3)Update a record")
             print("4)Remove a record")
             print("5)Enter back to main")
+            choice = main.force_int("Enter your choice : ")
             if choice == 1:
                 purchase_add()
             elif choice == 2:
@@ -156,6 +162,7 @@ while True:
             print("3)Update user information")
             print("4)Remove a user")
             print("5)Exit back to main")
+            choice = main.force_int("Enter your choice : ")
             if choice == 1:
                 user_add()
             elif choice == 2:
